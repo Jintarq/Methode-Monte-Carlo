44b1a7afadb96fe3582526925569db4d71f209a2
Jintarq <71939344+Jintarq@users.noreply.github.com>
2021-05-12 20:00:50 +0200

    Update on some mistakes.

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ n = 1000000
 
 # L'intervalle compris entre a et b, soit [a,b] :
 a = 0
-b = pi
+b = pi/2
 # On prend la valeur minimale sur l'intervalle.
 fmax = 0
 # On crée une liste des f_i(x) calculés.
 liste_f_i = []
-assert (a <= b)
+assert (a < b)
 for i in range(0, n):
     x = uniform(a,b)
     # On définie notre fonction et ses valeurs prises.
@@ -22,10 +22,10 @@ for i in range(0, n):
     # On remplie la liste avec les valeurs trouvées.
     liste_f_i.append(f_i)
     # On crée f_max(x), la valeur maximale pour pouvoir créer y. (On compare tous les f_i à la valeur minimale
-    # pour trouver la plus valeur max
+    # pour trouver la valeur max
     fmax = max(f_i, fmax)
-# On s'assure que les valeurs de f_i sont positives.
-assert (f_i >= 0)
+    # On s'assure que les valeurs de f_i sont positives.
+    assert (f_i >= 0)
 # On crée une boucle sur les valeurs de la liste et on les compare à y, si y est plus petit, alors il est
 # dans l'aire de la fonction.
 for f_i in liste_f_i:
@@ -36,5 +36,6 @@ for f_i in liste_f_i:
         c = c + 1
 
 # Le rapport établi entre la proportion de l'aire_courbe et l'aire_totale. (Points_courbes/Points_totaux).
-Aire_courbe = (c / n) * fmax
+Aire_courbe = (c / n) * ((b-a)*fmax)
+print(Aire_courbe)
 print(f"L'aire éstimée sur l'intervalle [{a}, {b}] est : {Aire_courbe} .")
